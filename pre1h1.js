612428b5c357a3023677e408bbc83e527c64ab21
nfcasas <126423724+nfcasas@users.noreply.github.com>
2023-03-07 00:48:51 -0300

    Add files via upload

---
+++ pre1h1.js
@@ -0,0 +1,49 @@
+function myFunction() {
+document.getElementById('pre1').style.display='block'
+document.getElementById('titulo1').style.display='block'
+document.getElementById('preinicio').style.display='none'
+document.getElementById('tituloinicio').style.display='none'
+document.getElementById('titulo2').style.display='none'
+document.getElementById('pre2').style.display='none'
+document.getElementById('formulario1').style.display='none'
+document.getElementById('pre3').style.display='none'
+
+
+}
+
+function myFunction1() {
+document.getElementById('pre1').style.display='none'
+document.getElementById('titulo1').style.display='none'
+document.getElementById('preinicio').style.display='block'
+document.getElementById('tituloinicio').style.display='block'
+document.getElementById('titulo2').style.display='none'
+document.getElementById('pre2').style.display='none'
+document.getElementById('formulario1').style.display='none'
+document.getElementById('pre3').style.display='none'
+
+
+
+}
+
+function myFunction2() {
+document.getElementById('pre1').style.display='none'
+document.getElementById('titulo1').style.display='none'
+document.getElementById('titulo2').style.display='block'
+document.getElementById('pre2').style.display='block'
+document.getElementById('preinicio').style.display='none'
+document.getElementById('tituloinicio').style.display='none'
+document.getElementById('formulario1').style.display='none'
+document.getElementById('pre3').style.display='none'
+
+}
+
+function myFunction3() {
+document.getElementById('pre1').style.display='none'
+document.getElementById('titulo1').style.display='none'
+document.getElementById('titulo2').style.display='none'
+document.getElementById('pre2').style.display='none'
+document.getElementById('preinicio').style.display='none'
+document.getElementById('tituloinicio').style.display='none'
+document.getElementById('formulario1').style.display='block'
+document.getElementById('pre3').style.display='block'
+}
