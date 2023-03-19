6cef9e9c647699c41f9f4fa99dca1f7db5b7cead
nfcasas <126423724+nfcasas@users.noreply.github.com>
2023-03-19 20:04:27 -0300

    Add files via upload

--- pre1h1.js
+++ pre1h1.js
@@ -1,49 +1,153 @@
-function myFunction() {
-document.getElementById('pre1').style.display='block'
-document.getElementById('titulo1').style.display='block'
-document.getElementById('preinicio').style.display='none'
-document.getElementById('tituloinicio').style.display='none'
-document.getElementById('titulo2').style.display='none'
-document.getElementById('pre2').style.display='none'
-document.getElementById('formulario1').style.display='none'
-document.getElementById('pre3').style.display='none'
-
-
-}
-
-function myFunction1() {
-document.getElementById('pre1').style.display='none'
-document.getElementById('titulo1').style.display='none'
-document.getElementById('preinicio').style.display='block'
-document.getElementById('tituloinicio').style.display='block'
-document.getElementById('titulo2').style.display='none'
-document.getElementById('pre2').style.display='none'
-document.getElementById('formulario1').style.display='none'
-document.getElementById('pre3').style.display='none'
-
-
-
-}
-
-function myFunction2() {
-document.getElementById('pre1').style.display='none'
-document.getElementById('titulo1').style.display='none'
-document.getElementById('titulo2').style.display='block'
-document.getElementById('pre2').style.display='block'
-document.getElementById('preinicio').style.display='none'
-document.getElementById('tituloinicio').style.display='none'
-document.getElementById('formulario1').style.display='none'
-document.getElementById('pre3').style.display='none'
-
-}
-
-function myFunction3() {
-document.getElementById('pre1').style.display='none'
-document.getElementById('titulo1').style.display='none'
-document.getElementById('titulo2').style.display='none'
-document.getElementById('pre2').style.display='none'
-document.getElementById('preinicio').style.display='none'
-document.getElementById('tituloinicio').style.display='none'
-document.getElementById('formulario1').style.display='block'
-document.getElementById('pre3').style.display='block'
-}
+if (screen.width > 600) {
+    function myFunction() {
+        
+    document.getElementById('pre1').style.display='block'
+    document.getElementById('titulo1').style.display='block'
+    document.getElementById('preinicio').style.display='none'
+    document.getElementById('tituloinicio').style.display='none'
+    document.getElementById('titulo2').style.display='none'
+    document.getElementById('pre2').style.display='none'
+    document.getElementById('formulario1').style.display='none'
+    document.getElementById('pre3').style.display='none'
+    
+    
+    }
+    
+    function myFunction1() {
+    document.getElementById('pre1').style.display='none'
+    document.getElementById('titulo1').style.display='none'
+    document.getElementById('preinicio').style.display='block'
+    document.getElementById('tituloinicio').style.display='block'
+    document.getElementById('titulo2').style.display='none'
+    document.getElementById('pre2').style.display='none'
+    document.getElementById('formulario1').style.display='none'
+    document.getElementById('pre3').style.display='none'
+    
+    
+    
+    }
+    
+    function myFunction2() {
+    document.getElementById('pre1').style.display='none'
+    document.getElementById('titulo1').style.display='none'
+    document.getElementById('titulo2').style.display='block'
+    document.getElementById('pre2').style.display='block'
+    document.getElementById('preinicio').style.display='none'
+    document.getElementById('tituloinicio').style.display='none'
+    document.getElementById('formulario1').style.display='none'
+    document.getElementById('pre3').style.display='none'
+    
+    }
+    
+    function myFunction3() {
+    document.getElementById('pre1').style.display='none'
+    document.getElementById('titulo1').style.display='none'
+    document.getElementById('titulo2').style.display='none'
+    document.getElementById('pre2').style.display='none'
+    document.getElementById('preinicio').style.display='none'
+    document.getElementById('tituloinicio').style.display='none'
+    document.getElementById('formulario1').style.display='block'
+    document.getElementById('pre3').style.display='block'
+    }
+    }
+    else {
+        function myFunction() {
+        
+        document.getElementById('pre1').style.display='block'
+        document.getElementById('titulo1').style.display='block'
+        document.getElementById('preinicio').style.display='none'
+        document.getElementById('tituloinicio').style.display='none'
+        document.getElementById('titulo2').style.display='none'
+        document.getElementById('pre2').style.display='none'
+        document.getElementById('formulario1').style.display='none'
+        document.getElementById('pre3').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('tituloacerca').style.display='none'
+        document.getElementById('ACERCA').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('link2').style.display='block'
+        document.getElementById('link3').style.display='none'
+        document.getElementById('link4').style.display='Block'
+        document.getElementById('link5').style.display='block'
+        
+        }
+        
+        function myFunction1() {
+        document.getElementById('pre1').style.display='none'
+        document.getElementById('titulo1').style.display='none'
+        document.getElementById('preinicio').style.display='block'
+        document.getElementById('tituloinicio').style.display='block'
+        document.getElementById('titulo2').style.display='none'
+        document.getElementById('pre2').style.display='none'
+        document.getElementById('formulario1').style.display='none'
+        document.getElementById('pre3').style.display='none'    
+        document.getElementById('ACERCA').style.display='none'
+        document.getElementById('tituloacerca').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('link2').style.display='none'
+        document.getElementById('link3').style.display='block'
+        document.getElementById('link4').style.display='Block'
+        document.getElementById('link5').style.display='block'
+        
+        }
+        
+        function myFunction2() {
+        document.getElementById('pre1').style.display='none'
+        document.getElementById('titulo1').style.display='none'
+        document.getElementById('titulo2').style.display='block'
+        document.getElementById('pre2').style.display='block'
+        document.getElementById('preinicio').style.display='none'
+        document.getElementById('tituloinicio').style.display='none'
+        document.getElementById('formulario1').style.display='none'
+        document.getElementById('pre3').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('ACERCA').style.display='none'
+        document.getElementById('tituloacerca').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('link2').style.display='block'
+        document.getElementById('link3').style.display='block'
+        document.getElementById('link4').style.display='none'
+        document.getElementById('link5').style.display='block'
+        
+        
+        }
+        
+        function myFunction3() {
+        document.getElementById('pre1').style.display='none'
+        document.getElementById('titulo1').style.display='none'
+        document.getElementById('titulo2').style.display='none'
+        document.getElementById('pre2').style.display='none'
+        document.getElementById('preinicio').style.display='none'
+        document.getElementById('tituloinicio').style.display='none'
+        document.getElementById('formulario1').style.display='block'
+        document.getElementById('pre3').style.display='block'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('ACERCA').style.display='none'
+        document.getElementById('tituloacerca').style.display='none'
+        document.getElementById('link1').style.display='block'
+        document.getElementById('link2').style.display='block'
+        document.getElementById('link3').style.display='block'
+        document.getElementById('link4').style.display='block'
+        document.getElementById('link5').style.display='none'
+        document.getElementById('piedepagina').style.display='none'
+    }
+    function myFunction4() {
+        document.getElementById('pre1').style.display='none'
+        document.getElementById('titulo1').style.display='none'
+        document.getElementById('titulo2').style.display='none'
+        document.getElementById('pre2').style.display='none'
+        document.getElementById('preinicio').style.display='none'
+        document.getElementById('tituloinicio').style.display='none'
+        document.getElementById('formulario1').style.display='none'
+        document.getElementById('pre3').style.display='none'
+        document.getElementById('link1').style.display='none'
+        document.getElementById('ACERCA').style.display='block'
+        document.getElementById('tituloacerca').style.display='block'    
+        document.getElementById('link1').style.display='none'
+        document.getElementById('link2').style.display='block'
+        document.getElementById('link3').style.display='block'
+        document.getElementById('link4').style.display='block'
+        document.getElementById('link5').style.display='block'
+    }
+    
+    }
